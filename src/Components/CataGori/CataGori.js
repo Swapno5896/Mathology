6cf9347094781ba06424070b8004adab5251bd52
swaono5896 <swapnom73@gmail.com>
2022-09-25 12:14:21 +0600

    catagori text decorated

--- src/Components/CataGori/CataGori.js
+++ src/Components/CataGori/CataGori.js
@@ -27,14 +27,36 @@ const CataGori = () => {
     return (
         <div className='container mb-5 pb-5'>
             <h3 className='text-center mt-5 mb-5 catagori-welcome'>Welcome To CALCHUNT 1.0</h3>
-            <p className='pb-3 fs-5'>There are 15 questions in total.
-                You will get only 45 minutes to answer. After 45 minutes it will be closed automatically and you can't able to submit the form and will be disqualified. So Be vigilant about times.
-                If you just answer the in the text box you will get only 50% marks.
-                To get 100% marks you have to send us your solutions to the problems on our email account + write down the answer in the text box. You have to specify the problems. You can submit in any format except videos.
+            <p className='pb-3 fs-5'> <span className='text-primary fs-3 '>
+                Dear Participant
 
-                You must submit handwritten images. Any image from the Internet, copy or any image including typing is prohibited and this will not be counted in marking.
-                We know some problems are difficult to type, for that kind of problems if you send us the solution we will be eligible for getting full marks.
-                <br />    Email ID: calculusoftheday@gmail.com</p>
+            </span>
+                <br />
+                <br />
+                You have got all day to take part in the event. You can part in the exam at any part of the day.
+                There are <span className='text-success'>
+                    15 questions
+                </span>  in total.
+
+
+                <span className='text-warning'>
+                    You will get only 45 minutes to answer,  After 45 minutes it will be closed automatically, and you can't be able to submit the form and will be disqualified.
+                </span>
+
+
+                So Be vigilant about times. If you just answer the in the text box you will get only
+                <span className='text-danger'>
+                    50% marks
+                </span>
+                . To get 100% marks, you have to send us your solutions to the problems on our email account + write down the answer in the text box. You have to specify the solutions in your exam script.
+                <span className='text-warning'>
+                    You can submit in any format(must have to be hand written) except videos.
+                </span>
+                Any image from the Internet, copy or any image including typing is prohibited and this will not be counted in marking. We know some problems are difficult to type, for that kind of problems if you send us the solution you will be eligible to get full marks.
+                <br />   <span className='fs-4 text-info '>
+                    Email ID: calculusoftheday@gmail.com
+                </span>
+            </p>
             <div className="row">
                 <div className="col-md-6 col-sm-12">
                     <img src={require('../../images/Cover 1.png')} class="img-fluid mt-5" alt="..." />
@@ -56,7 +78,7 @@ const CataGori = () => {
                         <select onChange={handelChange} class="form-select" id="segment">
                             <option selected>Choose Your Segment</option>
                             <option value="integration">Integration Bee  </option>
-                            {/* <option value="differentiation">Differentiation Bee   </option> */}
+                            <option value="differentiation">Differentiation Bee   </option>
                             <option value="application-of-calculu">Application of Calculus </option>
                             <option value="math-olympiad">Math Olympiad </option>
                             <option value="mean-contest">Mathematical Meme Contest</option>
